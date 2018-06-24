48d20bcaeb6fc267c9187ce301a0fa3d49b8aae3
AMCorvi <am@corvi.com>
2018-06-24 18:51:18 -0400

    -Added: [blog/generatory.py] path variables in post route

--- blog/generator.py
+++ blog/generator.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from werkzeug import cached_property
+import os
+import markdown
 
+MARKDOWN_EXTENSION = ".md"
 app = Flask(__name__)
+
+
+class Post(object):
+    def __init__(self, path):
+        self.path = path
+
+    @cached_property
+    def html(self):
+        with open(self.path, "r") as fin:
+            content = fin.read().strip()
+        return markdown.markdown(content)
 
 
 @app.route("/")
@@ -8,10 +23,12 @@ def index():
     return "Hello World"
 
 
-@app.route("/blog/post")
-def post():
-    return render_template('post.html', post_content="Hola Mundo! (de templato)" )
+@app.route("/blog/<path:path>")
+def post(path):
+    path = os.path.join("posts", path + MARKDOWN_EXTENSION)
+    post_content = Post(path)
+    return render_template('post.html', post=post_content)
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
