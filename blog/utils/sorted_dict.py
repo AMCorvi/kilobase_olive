8655adc2db26286f00957a6c1445f7e839ebc9f3
AMCorvi <am@corvi.com>
2018-06-27 00:34:08 -0400

    -Added: [blog/utils/sorted_dict] adhoc Dictionary implementiation for managing post order

---
+++ blog/utils/sorted_dict.py
@@ -0,0 +1,38 @@
+import collections
+
+
+class SortedDict(collections.MutableMapping):
+    def __init__(self, key=None, reverse=False, items=None):
+        self._items = {}
+        self._keys = []
+        if key:
+            self._key_fn = lambda k: key(self._items[k])
+        else:
+            self._key_fn = lambda k: self._items[k]
+        self._reverse = reverse
+
+        if items is not None:
+            self.update(items)
+
+    def __getitem__(self, key):
+        return self._items[key]
+
+    def __setitem__(self, key, value):
+        self._items[key] = value
+        if key not in self._keys:
+            self._keys.append(key)
+            self._keys.sort(key=self._key_fn, reverse=self._reverse)
+
+    def __delitem__(self, key):
+        self._items.pop(key)
+        self._keys.remove(key)
+
+    def __len__(self):
+        return len(self._keys)
+
+    def __iter__(self):
+        for key in self._keys:
+            yield key
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self._items)
