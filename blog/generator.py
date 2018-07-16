0c99a63bf746e6e2a0a00f9005d18dca1caa8ca1
AMCorvi <am@corvi.com>
2018-07-15 21:53:13 -0400

    - Modified: [blog/templates/index.html] Moved includes into - Modified: [blog/templates/includes/*] Moved Element from index.html distinctive partials

--- blog/generator.py
+++ blog/generator.py
@@ -1,5 +1,6 @@
 import os
-from flask import render_template, Flask
+from flask import render_template, Flask, request
+from werkzeug.contrib.atom import AtomFeed
 from .utils.blog import Blog
 
 MARKDOWN_EXTENSION = ".md"
@@ -7,7 +8,6 @@ POST_DIR = os.path.join("blog", "posts")
 
 app = Flask(__name__)
 blog = Blog(app, root_dir=os.path.relpath(POST_DIR))
-
 
 
 @app.template_filter("date")
@@ -20,7 +20,25 @@ def index():
     return render_template("index.html", posts=blog.posts)
 
 
-@app.route("/blog/<path:path>")
+@app.route("/blog/<path:path>/")
 def post(path):
     post = blog.get_post_or_404(path)
     return render_template('post.html', post=post)
+
+
+@app.route('/feed.atom/')
+def feed():
+    feed = AtomFeed(
+        'Recent Articles', feed_url=request.url, url=request.url_root)
+    posts = blog.posts
+    title = lambda p: "{}: {}".format(p.title, p.subtitle) if hasattr(p, 'subtitle') else p.title
+    for post in posts:
+        feed.add(
+            title(post),
+            bytes(post.html, "utf-8"),
+            content_type='html',
+            author='AMCorvi',
+            url=post.url(_external=True),
+            updated=post.date,
+            published=post.date)
+    return feed.get_response()
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from blog.generator import app
+import sys
+
+from flask_frozen import Freezer
+from blog.generator import app, blog
+
+freezer = Freezer(app)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        post_files = [post.filepath for post in blog.posts]
+        app.run(port=8000, debug=True, extra_files=post_files)
